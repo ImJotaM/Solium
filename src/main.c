a1ea610af2217040fe0abe0f70ec2dea2de51917
Joao Marcos Souza Fernandes <jo0209.letras@gmail.com>
2025-02-04 17:49:02 -0300

    Trying better app layout

--- src/main.c
+++ src/main.c
@@ -4,15 +4,79 @@
 #include <stdlib.h>
 #include <raylib.h>
 
-#define MAX_FILE_BUFFER 1024
+static void CreateWindow(char* fname, int width, int height) {
 
-int main() {
+	const char* prefix = "Solium - Content (";
+	const char* filename = fname;
+	const char* suffix = ")";
+
+	size_t buf = strlen(prefix) + strlen(filename) + strlen(suffix) + 1;
+
+	char* title = (char*)malloc(buf);
+
+	if (title == NULL) {
+		printf("Failed to allocate memory: %s", strerror(errno));
+		return;
+	}
+
+	strcpy(title, prefix);
+	strcat(title, filename);
+	strcat(title, suffix);
+
+	InitWindow(width, height, title);
+	SetTargetFPS(60);
+
+	free(title);
+}
+
+static bool FileExists(const char* filepath) {
+
+	FILE* tf = fopen(filepath, "r");
+	
+	if (tf) {
+		fclose(tf);
+		return true;
+	} else {
+		return false;
+	}
+
+}
+
+static void LoadMainFont(Font* font, const char* fontpath, int fontsize) {
+
+	if (FileExists(fontpath)) {
+		*font = LoadFontEx(fontpath, fontsize, NULL, NULL);
+	} else {
+		printf("Custom font not found. Using default function.");
+		*font = GetFontDefault();
+	}
+
+}
+
+int main(int argc, char* argv[]) {
+
+	if (argc != 2) {
+
+		printf("Wrong number of arguments.\n");
+		printf("Usage: solium <file>\n");
+
+		return -1;
+	}
 
 	int width = 800;
 	int height = 600;
-	const char* title = "Solium";
+	
+	CreateWindow(argv[1], width, height);
+	
+	const char* fontpath = "rsc/Consolas.ttf";
+	Font* font = (Font*)malloc(sizeof(struct Font));
+	if (font == NULL) {
+		printf("Failed to allocate memory: %s", strerror(errno));
+		return -1;
+	}
+	float fontsize = 16.f;
 
-	InitWindow(width, height, title);
+	LoadMainFont(font, fontpath, (int)fontsize);
 
 	FILE* file = NULL;
 	const char* filepath = "File.txt";
@@ -31,14 +95,15 @@ int main() {
 
 	if (fsize > 0) {
 
-		fcontent = (char*) malloc(fsize + 1);
+		fcontent = (char*) malloc(fsize);
 
 		if (fcontent != NULL) {
 			fread(fcontent, fsize, 1, file);
-			fcontent[fsize] = '\0';
+			fcontent[fsize - 1] = '\0';
 		}
 
 	} else {
+		
 		TraceLog(LOG_INFO, "File '%s' is empty", filepath);
 		return 0;
 	}
@@ -49,12 +114,13 @@ int main() {
 
 		ClearBackground(BLACK);
 
-		DrawText(fcontent, 0, 0, 16, WHITE);
+		DrawTextEx(*font, fcontent, (Vector2){ 0, 0 }, fontsize, 1.f , WHITE);
 		
 		EndDrawing();
 
 	}
 
+	free(font);
 	free(fcontent);
 	fclose(file);
 
